ed2d2bf17fe26bf8d486929f6f0ac6506fbb49ae
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-09 19:16:11 -0500

    refactored code

--- func_validator/_validator.py
+++ func_validator/_validator.py
@@ -3,84 +3,69 @@ from operator import ge, le, gt, lt, eq, contains
 
 
 # Numeric validation functions
-def MustBePositive(value, /, exc_type=ValueError, exc_msg=""):
-    exc_msg = exc_msg or f"Value {value} must be greater than 0."
-    if not gt(value, 0):
-        raise exc_type(exc_msg)
+def MustBePositive(value, /):
+    exc_msg = f"Value {value} must be greater than 0."
+    if not gt(value, 0): raise ValueError(exc_msg)
 
 
-def MustBeNonPositive(value, /, exc_type=ValueError, exc_msg=""):
-    exc_msg = exc_msg or f"Value {value} must be less than or equal to 0."
-    if not le(value, 0):
-        raise exc_type(exc_msg)
+def MustBeNonPositive(value, /):
+    exc_msg = f"Value {value} must be less than or equal to 0."
+    if not le(value, 0): raise ValueError(exc_msg)
 
 
-def MustBeNonNegative(value, /, exc_type=ValueError, exc_msg=""):
-    exc_msg = exc_msg or f"Value {value} must be greater than or equal to 0."
-    if not ge(value, 0):
-        raise exc_type(exc_msg)
+def MustBeNonNegative(value, /):
+    exc_msg = f"Value {value} must be greater than or equal to 0."
+    if not ge(value, 0): raise ValueError(exc_msg)
 
 
-def MustBeNegative(value, /, exc_type=ValueError, exc_msg=""):
-    exc_msg = exc_msg or f"Value {value} must be less than 0."
-    if not lt(value, 0):
-        raise exc_type(exc_msg)
+def MustBeNegative(value, /):
+    exc_msg = f"Value {value} must be less than 0."
+    if not lt(value, 0): raise ValueError(exc_msg)
 
 
 # Comparison validation functions
 
-def _comparison_validator(value, *, to, fn, symbol, exc_type, exc_msg):
-    if not fn(value, to):
-        raise exc_type(exc_msg)
+def _comparison_validator(value, *, to, fn, symbol):
+    exc_msg = f"Value {value} must be {symbol} {to}."
+    if not fn(value, to): raise ValueError(exc_msg)
 
 
-def MustBeEqual(value, /, exc_type=ValueError, exc_msg=""):
-    return partial(_comparison_validator, to=value, fn=eq, symbol="==",
-                   exc_type=exc_type, exc_msg=exc_msg)
+def MustBeEqual(value, /):
+    return partial(_comparison_validator, to=value, fn=eq, symbol="==")
 
 
-def MustBeGreaterThan(value, /, exc_type=ValueError, exc_msg=""):
-    return partial(_comparison_validator, to=value, fn=gt, symbol=">",
-                   exc_type=exc_type, exc_msg=exc_msg)
+def MustBeGreaterThan(value, /):
+    return partial(_comparison_validator, to=value, fn=gt, symbol=">")
 
 
-def MustBeLessThan(value, /, exc_type=ValueError, exc_msg=""):
-    return partial(_comparison_validator, to=value, fn=lt, symbol="<",
-                   exc_type=exc_type, exc_msg=exc_msg)
+def MustBeLessThan(value, /):
+    return partial(_comparison_validator, to=value, fn=lt, symbol="<")
 
 
-def MustBeGreaterThanOrEqual(value, /, exc_type=ValueError, exc_msg=""):
-    return partial(_comparison_validator, to=value, fn=ge, symbol=">=",
-                   exc_type=exc_type, exc_msg=exc_msg)
+def MustBeGreaterThanOrEqual(value, /):
+    return partial(_comparison_validator, to=value, fn=ge, symbol=">=")
 
 
-def MustBeLessThanOrEqual(value, /, exc_type=ValueError, exc_msg=""):
-    return partial(_comparison_validator, to=value, fn=le, symbol="<=",
-                   exc_type=exc_type, exc_msg=exc_msg)
+def MustBeLessThanOrEqual(value, /):
+    return partial(_comparison_validator, to=value, fn=le, symbol="<=")
 
 
 # Membership and range validation functions
 
-def MustBeIn(value_set, /, exc_type=ValueError, exc_msg=""):
+def MustBeIn(value_set, /):
     def f(value):
-        global exc_msg
-        exc_msg = exc_msg or f"Value {value} must be in {value_set}"
-        if not exc_msg:
-            exc_msg = f"Value {value} must be in {value_set}."
-        if not contains(value_set, value):
-            raise exc_type(exc_msg)
+        exc_msg = f"Value {value} must be in {value_set}"
+        if not contains(value_set, value): raise ValueError(exc_msg)
 
     return f
 
 
-def MustBeBetween(min_value, max_value, /, exc_type=ValueError, exc_msg=""):
+def MustBeBetween(*, min_value, max_value, /):
     def f(value):
-        global exc_msg
-        exc_msg = exc_msg or (f"Value {value} must be between "
-                              f"{min_value} and {max_value}.")
-
+        exc_msg = (f"Value {value} must be between "
+                   f"{min_value} and {max_value}.")
         if not (ge(value, min_value) and le(value, max_value)):
-            raise exc_type(exc_msg)
+            raise ValueError(exc_msg)
 
     return f
 
