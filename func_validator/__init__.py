aa85b3762fc479f21ffbcc279f6fb8036e110b31
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-09 22:37:29 -0500

    bumped version to v0.7.0

--- func_validator/__init__.py
+++ func_validator/__init__.py
@@ -4,7 +4,7 @@ from ._validator import (MustBePositive, MustBeNegative, MustBeNonNegative,
                          MustBeLessThan, MustBeIn, MustBeGreaterThanOrEqual,
                          MustBeLessThanOrEqual, MustBeBetween, MustBeNonEmpty)
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 __all__ = [
     "MustBePositive",
     "MustBeNonPositive",
