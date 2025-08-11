f3cd4561462062c6a58e50987a2d137f936dd853
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-11 15:49:53 -0500

    fixed check for if argument is annotated or not

--- func_validator/_func_validator.py
+++ func_validator/_func_validator.py
@@ -1,16 +1,26 @@
 import inspect
 from functools import wraps
-from typing import Callable, ParamSpec, TypeVar, get_type_hints, get_args, \
-    Annotated
+from typing import (
+    Callable,
+    ParamSpec,
+    TypeVar,
+    get_type_hints,
+    get_origin,
+    get_args,
+    Annotated,
+)
 
-P = ParamSpec('P')
-R = TypeVar('R')
+P = ParamSpec("P")
+R = TypeVar("R")
 
 
-def validate(func=None, /,
-             min_length: int | None = None,
-             max_length: int | None = None,
-             check_iterable_values=False):
+def validate(
+        func=None,
+        /,
+        min_length: int | None = None,
+        max_length: int | None = None,
+        check_iterable_values=False,
+):
     def dec(fn: Callable[P, R]) -> Callable[P, R]:
         @wraps(fn)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
@@ -21,29 +31,34 @@ def validate(func=None, /,
             func_type_hints = get_type_hints(fn, include_extras=True)
 
             for arg_name, arg_annotation in func_type_hints.items():
-                arg_metadata = get_args(arg_annotation)
-
-                if arg_name == 'return' or len(arg_metadata) < 2:
+                if arg_name == "return" or get_origin(
+                        arg_annotation) is not Annotated:
                     continue
 
-                _, *arg_validator_funcs = arg_metadata
+                _, *arg_validator_funcs = get_args(arg_annotation)
                 arg_value = arguments[arg_name]
 
                 for arg_validator_fn in arg_validator_funcs:
                     if not callable(arg_validator_fn):
-                        raise TypeError(f"Validator for argument '{arg_name}' "
-                                        f"is not callable: {arg_validator_fn}")
+                        raise TypeError(
+                            f"Validator for argument '{arg_name}' "
+                            f"is not callable: {arg_validator_fn}"
+                        )
 
                     if min_length is not None:
-                        exc_msg = (f"Length of argument '{arg_name}' "
-                                   f"must be at least {min_length}.")
                         if len(arg_value) < min_length:
+                            exc_msg = (
+                                f"Length of argument '{arg_name}' "
+                                f"must be at least {min_length}."
+                            )
                             raise ValueError(exc_msg)
 
                     if max_length is not None:
-                        exc_msg = (f"Length of argument '{arg_name}' "
-                                   f"must be at most {max_length}.")
                         if len(arg_value) > max_length:
+                            exc_msg = (
+                                f"Length of argument '{arg_name}' "
+                                f"must be at most {max_length}."
+                            )
                             raise ValueError(exc_msg)
 
                     if check_iterable_values:
@@ -66,16 +81,3 @@ def validate(func=None, /,
         return dec(func)
 
     raise TypeError("The first argument must be a callable function or None.")
-
-
-if __name__ == "__main__":
-    # Example usage of the validate decorator
-    @validate
-    def example_function(
-            arg1: list[int],
-            # arg2: Annotated[str, lambda x: x]
-    ):
-        pass
-
-
-    example_function("test")
