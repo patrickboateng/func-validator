da3479c37d74b695a4951100f00ce5271e8e1a89
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-14 23:52:08 -0500

    refactored code

--- func_validator/_func_validator.py
+++ func_validator/_func_validator.py
@@ -17,8 +17,6 @@ R = TypeVar("R")
 def validate_func_args_at_runtime(
     func=None,
     /,
-    min_length: int | None = None,
-    max_length: int | None = None,
     check_iterable_values=False,
 ):
     def dec(fn: Callable[P, R]) -> Callable[P, R]:
@@ -43,22 +41,6 @@ def validate_func_args_at_runtime(
                             f"Validator for argument '{arg_name}' "
                             f"is not callable: {arg_validator_fn}"
                         )
-
-                    if min_length is not None:
-                        if len(arg_value) < min_length:
-                            exc_msg = (
-                                f"Length of argument '{arg_name}' "
-                                f"must be at least {min_length}."
-                            )
-                            raise ValueError(exc_msg)
-
-                    if max_length is not None:
-                        if len(arg_value) > max_length:
-                            exc_msg = (
-                                f"Length of argument '{arg_name}' "
-                                f"must be at most {max_length}."
-                            )
-                            raise ValueError(exc_msg)
 
                     if check_iterable_values:
                         for v in arg_value:
