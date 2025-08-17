c506243ced59542189cc53ce950dfed5e8a5c8c4
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-16 21:29:26 -0500

    added typehints to functions

--- func_validator/_validators.py
+++ func_validator/_validators.py
@@ -1,24 +1,27 @@
 from functools import partial
 from operator import ge, le, gt, lt, eq, ne, contains
+from typing import TypeAlias, Iterable, Sized
+
+Number: TypeAlias = int | float
 
 
 # Numeric validation functions
-def MustBePositive(value, /):
+def MustBePositive(value: Number, /):
     if not gt(value, 0):
         raise ValueError(f"Value {value} must be greater than 0.")
 
 
-def MustBeNonPositive(value, /):
+def MustBeNonPositive(value: Number, /):
     if not le(value, 0):
         raise ValueError(f"Value {value} must be less than or equal to 0.")
 
 
-def MustBeNegative(value, /):
+def MustBeNegative(value: Number, /):
     if not lt(value, 0):
         raise ValueError(f"Value {value} must be less than 0.")
 
 
-def MustBeNonNegative(value, /):
+def MustBeNonNegative(value: Number, /):
     if not ge(value, 0):
         exc_msg = f"Value {value} must be greater than or equal to 0."
         raise ValueError(exc_msg)
@@ -32,34 +35,34 @@ def _comparison_validator(value, *, to, fn, symbol):
         raise ValueError(f"Value {value} must be {symbol} {to}.")
 
 
-def MustBeEqual(value, /):
+def MustBeEqual(value: Number, /):
     return partial(_comparison_validator, to=value, fn=eq, symbol="==")
 
 
-def MustBeNotEqual(value, /):
+def MustBeNotEqual(value: Number, /):
     return partial(_comparison_validator, to=value, fn=ne, symbol="!=")
 
 
-def MustBeGreaterThan(value, /):
+def MustBeGreaterThan(value: Number, /):
     return partial(_comparison_validator, to=value, fn=gt, symbol=">")
 
 
-def MustBeGreaterThanOrEqual(value, /):
+def MustBeGreaterThanOrEqual(value: Number, /):
     return partial(_comparison_validator, to=value, fn=ge, symbol=">=")
 
 
-def MustBeLessThan(value, /):
+def MustBeLessThan(value: Number, /):
     return partial(_comparison_validator, to=value, fn=lt, symbol="<")
 
 
-def MustBeLessThanOrEqual(value, /):
+def MustBeLessThanOrEqual(value: Number, /):
     return partial(_comparison_validator, to=value, fn=le, symbol="<=")
 
 
 # Membership and range validation functions
 
 
-def MustBeIn(value_set, /):
+def MustBeIn(value_set: Iterable, /):
     def f(value):
         if not contains(set(value_set), value):
             raise ValueError(f"Value {value} must be in {set(value_set)}")
@@ -67,48 +70,51 @@ def MustBeIn(value_set, /):
     return f
 
 
-def MustBeBetween(*, min_value, max_value):
-    def f(value):
-        if not (ge(value, min_value) and le(value, max_value)):
-            exc_msg = f"Value {value} must be between " f"{min_value} and {max_value}."
-            raise ValueError(exc_msg)
+def _must_be_between(value, *, min_value: Number, max_value: Number):
+    if not (ge(value, min_value) and le(value, max_value)):
+        exc_msg = f"Value {value} must be between {min_value} and {max_value}."
+        raise ValueError(exc_msg)
 
-    return f
+
+def MustBeBetween(*, min_value: Number, max_value: Number):
+    return lambda value: _must_be_between(
+        value, min_value=min_value, max_value=max_value
+    )
 
 
 # Size validation functions
 
-def MustBeEmpty(value, /):
+
+def MustBeEmpty(value: Iterable, /):
     if value:
         raise ValueError(f"Value {value} must be empty.")
 
 
-def MustBeNonEmpty(value, /):
+def MustBeNonEmpty(value: Iterable, /):
     if not value:
         raise ValueError(f"Value {value} must not be empty.")
 
 
-def MustHaveLengthEqual(value, /):
-    def f(val):
+def MustHaveLengthEqual(value: int, /):
+    def f(val: Sized, /):
         if len(val) != value:
             raise ValueError(f"Length of {val} must be equal to {value}.")
+
     return f
 
 
-def MustHaveLengthGreaterThan(value, /):
-    def f(val):
+def MustHaveLengthGreaterThan(value: int, /):
+    def f(val: Sized, /):
         if not (len(val) > value):
             raise ValueError(f"Length of {val} must be equal to {value}")
 
     return f
 
 
-def MustHaveValuesBetween(*, min_value, max_value):
-    def f(values):
+def MustHaveValuesBetween(*, min_value: Number, max_value: Number):
+    def f(values: Iterable, /):
         for val in values:
-            if not (ge(val, min_value) and le(val, max_value)):
-                exc_msg = f"Value {val} must be between " f"{min_value} and {max_value}."
-                raise ValueError(exc_msg)
+            _must_be_between(val, min_value=min_value, max_value=max_value)
 
     return f
 
