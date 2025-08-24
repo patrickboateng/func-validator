cac7e7823d171666c0a79512f3b8146603753771
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-24 17:40:23 -0500

    converted function validator to a class validator

--- func_validator/_validators/_text_arg_validators.py
+++ func_validator/_validators/_text_arg_validators.py
@@ -2,64 +2,67 @@ import re
 from typing import Literal
 
 
-def MustMatchRegex(
-        regex: str | re.Pattern,
-        *,
-        match_type: Literal["match", "fullmatch", "search"] = "match",
-        flags: int | re.RegexFlag = 0,
-):
-    """Validates that the value matches the provided regular expression.
+class MustMatchRegex:
+    def __init__(
+            self,
+            regex: str | re.Pattern,
+            *,
+            match_type: Literal["match", "fullmatch", "search"] = "match",
+            flags: int | re.RegexFlag = 0,
+    ):
+        """Validates that the value matches the provided regular expression.
 
-    :param regex: The regular expression to validate.
-    :type regex: str | re.Pattern
+        :param regex: The regular expression to validate.
+        :type regex: str | re.Pattern
 
-    :param match_type: The type of match to perform. Must be one of
-                       'match', 'fullmatch', or 'search'.
-                       Default is 'match'.
-    :type match_type: Literal["match", "fullmatch", "search"]
+        :param match_type: The type of match to perform. Must be one of
+                           'match', 'fullmatch', or 'search'.
+                           Default is 'match'.
+        :type match_type: Literal["match", "fullmatch", "search"]
 
-    :param flags: Optional regex flags to modify the regex behavior.
-                  Default is 0 (no flags). if `regex` is a compiled
-                  Pattern, flags are ignored.
-                  See `re` module for available flags.
-    :type flags: int | re.RegexFlag
+        :param flags: Optional regex flags to modify the regex behavior.
+                      Default is 0 (no flags). if `regex` is a compiled
+                      Pattern, flags are ignored.
+                      See `re` module for available flags.
+        :type flags: int | re.RegexFlag
 
-    :raises ValueError: If the value does not match the regex pattern.
+        :raises ValueError: If the value does not match the regex pattern.
 
-    :return: A validator function that accepts a string and raises
-             ValueError if it does not match.
-    :rtype: Callable[[str], None]
-    """
-    if not isinstance(regex, re.Pattern):
-        regex_pattern = re.compile(regex, flags=flags)
-    else:
-        regex_pattern = regex
+        :return: A validator function that accepts a string and raises
+                 ValueError if it does not match.
+        :rtype: Callable[[str], None]
+        """
+        if not isinstance(regex, re.Pattern):
+            regex_pattern = re.compile(regex, flags=flags)
+        else:
+            regex_pattern = regex
 
-    match match_type:
-        case "match":
-            match_func = regex_pattern.match
-        case "fullmatch":
-            match_func = regex_pattern.fullmatch
-        case "search":
-            match_func = regex_pattern.search
-        case _:
-            raise TypeError(
-                "Invalid match_type. Must be one of 'match', "
-                "'fullmatch', or 'search'."
-            )
+        match match_type:
+            case "match":
+                match_func = regex_pattern.match
+            case "fullmatch":
+                match_func = regex_pattern.fullmatch
+            case "search":
+                match_func = regex_pattern.search
+            case _:
+                raise TypeError(
+                    "Invalid match_type. Must be one of 'match', "
+                    "'fullmatch', or 'search'."
+                )
 
-    def validator(value: str) -> None:
+        self.regex_pattern = regex_pattern
+        self.match_func = match_func
+
+    def __call__(self, value: str) -> None:
         """Validator function to check if the value matches the
         regex pattern.
         """
         if not isinstance(value, str):
             exc_msg = f"Value must be a string, got {type(value)} instead."
             raise TypeError(exc_msg)
-        if not match_func(value):
+        if not self.match_func(value):
             exc_msg = (
                 f"Value '{value}' does not match the "
-                f"regex pattern '{regex_pattern.pattern}'."
+                f"regex pattern '{self.regex_pattern.pattern}'."
             )
             raise ValueError(exc_msg)
-
-    return validator
