c5af5ec528e70a5bfe856291ead089ede242bd05
Patrick Boateng <boatengpato.pb@gmail.com>
2025-08-28 18:51:41 -0500

    added new test cases to achieve full module coverage

--- tests/test_datatype_validators.py
+++ tests/test_datatype_validators.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 import pytest
 
@@ -14,3 +14,22 @@ def test_must_be_a_validator():
 
     with pytest.raises(ValidationError):
         func((1, 2, 3))
+
+    @validate_func_args(check_arg_types=True)
+    def func_2(x_1: Annotated[Optional[int], MustBeA(int)]):
+        return x_1
+
+    assert func_2(2) == 2
+    assert func_2(None) is None
+
+    with pytest.raises(TypeError):
+        func_2((1, 2, 3))
+
+    with pytest.raises(TypeError):
+        validate_func_args("invalid")
+
+    @validate_func_args
+    def func_3(x_1: int, x_2: Annotated[int, "Just a Metadata"]):
+        return x_1, x_2
+
+    assert func_3(2, 3) == (2, 3)
