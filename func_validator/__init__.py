729749a94a5dd444fb3a2cc67f03fa0f0d305dc9
Patrick Boateng <boatengpato.pb@gmail.com>
2025-09-17 13:02:07 -0500

    simplied package imports

--- func_validator/__init__.py
+++ func_validator/__init__.py
@@ -2,77 +2,12 @@ from ._func_arg_validator import (
     validate_func_args,
     validate_func_args_at_runtime,
 )
-from .validators import (
-    MustBeBetween,
-    MustBeEmpty,
-    MustBeEqual,
-    MustBeGreaterThan,
-    MustBeGreaterThanOrEqual,
-    MustBeMemberOf,
-    MustBeLessThan,
-    MustBeLessThanOrEqual,
-    MustBeNegative,
-    MustBeNonEmpty,
-    MustBeMemberOf,
-    MustBeNonNegative,
-    MustBeNonPositive,
-    MustBeNotEqual,
-    MustBePositive,
-    MustHaveLengthBetween,
-    MustHaveLengthEqual,
-    MustHaveLengthGreaterThan,
-    MustHaveLengthGreaterThanOrEqual,
-    MustHaveLengthLessThan,
-    MustHaveLengthLessThanOrEqual,
-    MustHaveValuesBetween,
-    MustHaveValuesGreaterThan,
-    MustHaveValuesGreaterThanOrEqual,
-    MustHaveValuesLessThan,
-    MustHaveValuesLessThanOrEqual,
-    MustBeNotEqual,
-    MustMatchRegex,
-    MustMatchBSCAddress,
-    MustBeA,
-    ValidationError,
-)
+from .validators import *
+from . import validators
 
 __version__ = "0.13.0"
 
 __all__ = [
-    # Error
-    "ValidationError",
-    # Collection Validators
-    "MustBeMemberOf",
-    "MustBeEmpty",
-    "MustBeNonEmpty",
-    "MustHaveLengthEqual",
-    "MustHaveLengthGreaterThan",
-    "MustHaveLengthGreaterThanOrEqual",
-    "MustHaveLengthLessThan",
-    "MustHaveLengthLessThanOrEqual",
-    "MustHaveLengthBetween",
-    "MustHaveValuesGreaterThan",
-    "MustHaveValuesGreaterThanOrEqual",
-    "MustHaveValuesLessThan",
-    "MustHaveValuesLessThanOrEqual",
-    "MustHaveValuesBetween",
-    # DataType Validators
-    "MustBeA",
-    # Numeric Validators
-    "MustBePositive",
-    "MustBeNonPositive",
-    "MustBeNegative",
-    "MustBeNonNegative",
-    "MustBeBetween",
-    "MustBeEqual",
-    "MustBeNotEqual",
-    "MustBeGreaterThan",
-    "MustBeGreaterThanOrEqual",
-    "MustBeLessThan",
-    "MustBeLessThanOrEqual",
-    # Text Validators
-    "MustMatchRegex",
-    # decorators
-    "validate_func_args",
-    "validate_func_args_at_runtime",
-]
+              "validate_func_args",
+              "validate_func_args_at_runtime",
+          ] + validators.__all__
