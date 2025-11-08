8c8d3366b6c53f8273b7ef14d54b485e1d297aaa
Patrick Boateng <boatengpato.pb@gmail.com>
2025-11-08 00:33:38 -0600

    bumped version to 1.3.0

--- func_validator/__init__.py
+++ func_validator/__init__.py
@@ -2,5 +2,5 @@ from . import validators
 from ._func_arg_validator import validate_params
 from .validators import *
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 __all__ = ["validate_params"] + validators.__all__
